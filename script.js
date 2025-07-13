04c7fe4a3d93cd2bf2cea5f8c8e3148362e7d7d7
Rob Thacher <thacher@users.noreply.github.com>
2025-07-13 09:22:26 -0700

    updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Human","Father","Founder","Fintech Nerd","Inventor"],
+        strings: ["Human","Father","Founder","Fintech Nerd","AI Engineer","Inventor"],
         typeSpeed: 80,
         backSpeed: 50,
         cursorChar: ' |',
